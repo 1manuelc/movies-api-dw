07d9494613b42e4eddc2450d239be4b5cdb27fac
1manuelc <manuelwn21@gmail.com>
2025-05-24 21:08:55 -0300

    feat: refactor MovieService to use Sequelize for database operations

--- src/service/MoviesService.ts
+++ src/service/MoviesService.ts
@@ -1,67 +1,29 @@
-import { moviesMock } from '../mocks/movies-mock';
-import { MovieModel } from '../model/MovieModel';
+import { MovieDTO } from '../dtos/movie-dtos';
+import { Movie } from '../models/Movie';
 
 export class MovieService {
-	movies: MovieModel[];
-	nextId: number;
-
-	constructor({ useMock }: { useMock: boolean }) {
-		if (useMock) {
-			this.movies = moviesMock;
-			this.nextId = moviesMock.length + 1;
-		} else {
-			this.movies = [];
-			this.nextId = 1;
-		}
+	async getAll() {
+		const movies = await Movie.findAll();
+		return movies;
 	}
 
-	getAll() {
-		return this.movies;
-	}
-
-	getById(id: number) {
-		const movieMatch = this.movies.find((movie) => movie.id === id);
-
-		if (!movieMatch) {
-			throw new Error(`Movie of id ${id} not found`);
-		}
-
+	async getById(id: number) {
+		const movieMatch = await Movie.findByPk(id);
 		return movieMatch;
 	}
 
-	create(newMovie: Omit<MovieModel, 'id'>) {
-		this.movies.push(
-			new MovieModel({
-				id: this.nextId,
-				...newMovie,
-			})
-		);
-		this.nextId += 1;
-		return this.movies.at(-1);
+	async create(newMovie: MovieDTO) {
+		const createdMovie = Movie.create({ ...newMovie });
+		return createdMovie;
 	}
 
-	update({ id, newMovie }: { id: number; newMovie: Omit<MovieModel, 'id'> }) {
-		const matchIndex = this.movies.findIndex((movie) => movie.id === id);
-
-		if (matchIndex === -1) {
-			throw new Error(`Movie of id ${id} not found`);
-		}
-
-		this.movies[matchIndex] = new MovieModel({
-			...this.movies[matchIndex],
-			...newMovie,
-		});
-
-		return this.movies[matchIndex];
+	async update({ id, newMovie }: { id: number; newMovie: MovieDTO }) {
+		const movieMatch = await Movie.findByPk(id);
+		return await movieMatch?.update(newMovie);
 	}
 
-	remove(id: number) {
-		const matchIndex = this.movies.findIndex((movie) => movie.id === id);
-
-		if (matchIndex === -1) {
-			throw new Error(`Movie of id ${id} not found`);
-		}
-
-		return this.movies.splice(matchIndex, 1)[0];
+	async remove(id: number) {
+		const movieMatch = await Movie.findByPk(id);
+		return await movieMatch?.destroy();
 	}
 }
