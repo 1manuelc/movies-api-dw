d471846e0c83e82a18cfdb7a2e83e61771552bfd
1manuelc <manuelwn21@gmail.com>
2025-05-24 21:07:12 -0300

    feat: add MovieModel type definition for movie attributes

---
+++ src/@types/movie.d.ts
@@ -0,0 +1,10 @@
+export type MovieModel = {
+	id: number;
+	title: string;
+	description: string;
+	genre: string;
+	rating: number;
+	director: string;
+	releaseYear: number;
+	durationInSeconds: number;
+};
